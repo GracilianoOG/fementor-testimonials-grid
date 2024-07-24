6194ed167018b660a81c6aeb6626587b59c4d3ea
Gabriel <gabriel.gracilianomb@gmail.com>
2024-07-24 15:51:25 -0300

    Create card basic structure

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,23 @@
 import { StyledCard } from "./styles/StyledCard"
+import avatar from "../assets/images/image-daniel.jpg"
 
 const Card = () => {
   return (
     <StyledCard>
-      card
+      <div>
+        <img src={avatar} alt="" />
+        <div>
+          <h2>Daniel Clifford</h2>
+          <p>Verified Graduate</p>
+        </div>
+      </div>
+      <h3>I received a job offer mid-course, and the subjects I learned were current, if not more so,
+        in the company I joined. I honestly feel I got every penny’s worth.</h3>
+      <p>“ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a
+        transition and have heard some people who had an amazing experience here. I signed up
+        for the free intro course and found it incredibly fun! I enrolled shortly thereafter.
+        The next 12 weeks was the best - and most grueling - time of my life. Since completing
+        the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup. ”</p>
     </StyledCard>
   )
 }
