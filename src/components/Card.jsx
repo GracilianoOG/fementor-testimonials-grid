060f7a183f61cf016d2c4829da78736a95760bb6
Gabriel <gabriel.gracilianomb@gmail.com>
2024-07-24 16:21:09 -0300

    Add CSS classes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,16 +4,16 @@ import avatar from "../assets/images/image-daniel.jpg"
 const Card = () => {
   return (
     <StyledCard>
-      <div>
-        <img src={avatar} alt="" />
-        <div>
-          <h2>Daniel Clifford</h2>
-          <p>Verified Graduate</p>
+      <div className="header">
+        <img className="avatar" src={avatar} alt="" />
+        <div className="info">
+          <h2 className="name">Daniel Clifford</h2>
+          <p className="status">Verified Graduate</p>
         </div>
       </div>
-      <h3>I received a job offer mid-course, and the subjects I learned were current, if not more so,
+      <h3 className="title">I received a job offer mid-course, and the subjects I learned were current, if not more so,
         in the company I joined. I honestly feel I got every penny’s worth.</h3>
-      <p>“ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a
+      <p className="description">“ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a
         transition and have heard some people who had an amazing experience here. I signed up
         for the free intro course and found it incredibly fun! I enrolled shortly thereafter.
         The next 12 weeks was the best - and most grueling - time of my life. Since completing
