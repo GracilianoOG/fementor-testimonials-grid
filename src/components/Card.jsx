3693e0208601cf32c916d0680f22e8107fb50e9f
Gabriel <gabriel.gracilianomb@gmail.com>
2024-07-26 11:10:54 -0300

    Add default value for status

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({ name, status, title, review, avatar }) => {
         <img className="avatar" src={avatar} alt="" />
         <div className="info">
           <h2 className="name">{ name }</h2>
-          <p className="status">{ status }</p>
+          <p className="status">{ status || "Verified Graduate" }</p>
         </div>
       </div>
       <h3 className="title">{ title }</h3>
