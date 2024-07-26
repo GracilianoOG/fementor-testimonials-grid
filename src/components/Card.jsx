c932b324d16f21bab51fa2b1c6e6400e8544dedb
Gabriel <gabriel.gracilianomb@gmail.com>
2024-07-26 10:44:19 -0300

    Add props for textual content

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,23 +1,18 @@
 import { StyledCard } from "./styles/StyledCard"
 import avatar from "../assets/images/image-daniel.jpg"
 
-const Card = () => {
+const Card = ({ name, status, title, review }) => {
   return (
     <StyledCard>
       <div className="header">
         <img className="avatar" src={avatar} alt="" />
         <div className="info">
-          <h2 className="name">Daniel Clifford</h2>
-          <p className="status">Verified Graduate</p>
+          <h2 className="name">{ name }</h2>
+          <p className="status">{ status }</p>
         </div>
       </div>
-      <h3 className="title">I received a job offer mid-course, and the subjects I learned were current, if not more so,
-        in the company I joined. I honestly feel I got every penny’s worth.</h3>
-      <p className="description">“ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a
-        transition and have heard some people who had an amazing experience here. I signed up
-        for the free intro course and found it incredibly fun! I enrolled shortly thereafter.
-        The next 12 weeks was the best - and most grueling - time of my life. Since completing
-        the course, I’ve successfully switched careers, working as a Software Engineer at a VR startup. ”</p>
+      <h3 className="title">{ title }</h3>
+      <p className="description">{ review }</p>
     </StyledCard>
   )
 }
