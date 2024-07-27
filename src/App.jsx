c968004ce187290034ab1c53795adf91c00cfc49
Gabriel <gabriel.gracilianomb@gmail.com>
2024-07-27 19:58:52 -0300

    Add fifth card

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import danielAvatar from "./assets/images/image-daniel.jpg"
 import jonathanAvatar from "./assets/images/image-jonathan.jpg"
 import jeanetteAvatar from "./assets/images/image-jeanette.jpg"
 import patrickAvatar from "./assets/images/image-patrick.jpg"
+import kiraAvatar from "./assets/images/image-kira.jpg"
 
 const App = () => {
   return (
@@ -42,6 +43,19 @@ const App = () => {
         gave me the confidence necessary to be able to go out in the world and present myself as a capable 
         junior developer. The standard is above the rest. You will get the personal attention you need from 
         an incredible community of smart and amazing people."
+      />
+      <Card
+        avatar={ kiraAvatar }
+        name="Kira Whittle"
+        title="Such a life-changing experience. Highly recommended!"
+        review="Before joining the bootcamp, I’ve never written a line of code. I needed some structure from 
+        professionals who can help me learn programming step by step. I was encouraged to enroll by a former 
+        student of theirs who can only say wonderful things about the program. The entire curriculum and staff 
+        did not disappoint. They were very hands-on and I never had to wait long for assistance. The agile team 
+        project, in particular, was outstanding. It took my learning to the next level in a way that no tutorial 
+        could ever have. In fact, I’ve often referred to it during interviews as an example of my developent 
+        experience. It certainly helped me land a job as a full-stack developer after receiving multiple offers. 
+        100% recommend!"
       />
     </div>
   )
