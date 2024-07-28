5e214426f11533b893b5aa232a5040202d7f44c9
Gabriel <gabriel.gracilianomb@gmail.com>
2024-07-28 16:05:01 -0300

    Set colors from props and default colors

--- src/components/styles/StyledCard.jsx
+++ src/components/styles/StyledCard.jsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const StyledCard = styled.article`
-  background-color: hsl(263, 55%, 52%);
+  background-color: ${(props) => props?.cardColors?.bg || "hsl(0, 0%, 100%)"};
   border-radius: 8px;
   max-width: 326px;
   padding: 32px;
@@ -10,7 +10,7 @@ export const StyledCard = styled.article`
   .name,
   .status,
   .description {
-    color: hsl(0, 0%, 81%);
+    color: ${(props) => props?.cardColors?.text || "hsl(217, 19%, 35%)"};
   }
 
   .header {
@@ -22,7 +22,7 @@ export const StyledCard = styled.article`
 
   .avatar {
     border-radius: 50%;
-    outline: 2px solid #9971d7;
+    outline: 2px solid ${(props) => props?.cardColors?.avatar || "hsl(0, 0%, 100%)"};
     width: 1.75rem;
   }
 
@@ -36,7 +36,7 @@ export const StyledCard = styled.article`
   }
 
   .title {
-    color: hsl(0, 0%, 100%);
+    color: ${(props) => props?.cardColors?.title || "hsl(217, 19%, 35%)"};
     font-size: 1.25rem;
     margin-bottom: 1rem;
   }
