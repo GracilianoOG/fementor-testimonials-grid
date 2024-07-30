a787fb392933e51dc53fa04bf9d97d894c6ae7e5
Gabriel <gabriel.gracilianomb@gmail.com>
2024-07-30 18:26:06 -0300

    Add wrapper component for styled cards

---
+++ src/components/styles/StyledCardWrapper.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledCardWrapper = styled.div`
+  display: grid;
+  gap: 23px 29px;
+
+  @media screen and (min-width: 992px) {
+    & {
+      grid-template-areas: 
+      "card1 card1 card2 card3"
+      "card4 card5 card5 card3";
+    }
+  }
+`
